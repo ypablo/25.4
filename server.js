04e149cc66a19b68dd701f853b4297acfda98aa8
ypablo <yfedora@gmail.com>
2019-06-07 17:22:09 +0100

    25.4 Serwowanie plików statycznych

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/* var express = require('express');
 var app = express();
 var fs = require('fs')
 var stringifyFile;
@@ -23,4 +23,27 @@ app.post('/updateNote/:note', function (req, res) {
     res.send(stringifyFile);
 });
 
-var server = app.listen(3000);
+var server = app.listen(3000); */
+
+var express = require('express');
+var app = express();
+app.use(express.static('assets'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html')
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.json(response);
+});
+
+var server = app.listen(3000, 'localhost', function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
